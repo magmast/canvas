7052027b57aaf0472cb75994ccb704776676b73f
Maciej Augustyniak <maciej@augustyniak.app>
2025-04-26 07:36:23 +0200

    refactor: extract the canvas model to a separate class

--- src/canvas/__init__.py
+++ src/canvas/__init__.py
@@ -1,19 +1,60 @@
 from contextlib import ExitStack
+from dataclasses import dataclass, field
 from typing import Self
 
 from blessed import Terminal
 
 
 def write(*args: object) -> None:
-    """Wrapper over the `print` function that sets the `end` argument to `None` and flushes the buffer immediately."""
+    """Wrapper over the `print` function that sets the `end` argument to `None`
+    and flushes the buffer immediately."""
 
     print(*args, end="", flush=True)
+
+
+@dataclass
+class Canvas:
+    blocks: set[tuple[int, int]] = field(default_factory=set)
+    size: tuple[int, int] = (50, 20)
+    cursor: tuple[int, int] = (0, 0)
+
+    def move_cursor(self, dx: int, dy: int) -> set[tuple[int, int]]:
+        """Move the cursor by the given offsets, clamping the result within the
+        canvas bounds.
+
+        Returns:
+            A set of positions that should be rerendered after the update.
+        """
+
+        width, height = self.size
+        x, y = self.cursor
+        new_x = max(0, min(width - 1, x + dx))
+        new_y = max(0, min(height - 1, y + dy))
+        self.cursor = (new_x, new_y)
+
+        if (x, y) != self.cursor:
+            return set([(x, y), self.cursor])
+        else:
+            return set()
+
+    def toggle_block(self) -> set[tuple[int, int]]:
+        """Toggles the block pointed by the cursor.
+
+        Returns:
+            A set of positions that should be rerendered after the update."""
+
+        if self.cursor in self.blocks:
+            self.blocks.remove(self.cursor)
+        else:
+            self.blocks.add(self.cursor)
+
+        return set([self.cursor])
 
 
 class CanvasController:
     """Manages a printable canvas in a terminal window.
 
-    Canvas can be controlled by the user through the following list of actions:
+    Canvas can be controlled by a user through the following actions:
 
     - 'w': Move the cursor up.
     - 'd': Move the cursor to the right.
@@ -26,38 +67,33 @@ class CanvasController:
         self,
         *,
         term: Terminal = Terminal(),
-        width: int = 50,
-        height: int = 20,
+        canvas: Canvas = Canvas(blocks=set()),
         cursor="█",
         block="█",
     ) -> None:
         self._stack = ExitStack()
         self.term = term
-        self.width = width
-        self.height = height
+        self.canvas = canvas
         self.cursor = cursor
         self.block = block
         self._quitting = False
-        self.x = 0
-        self.y = 0
-        self.blocks: set[tuple[int, int]] = set()
-        self.updates: list[tuple[int, int]] = []
 
     def __enter__(self) -> Self:
         self._stack.enter_context(self.term.cbreak())
         self._stack.enter_context(self.term.hidden_cursor())
-        write(self.term.home + self.term.clear)
         return self
 
     def __exit__(self, *args: any) -> None:
         self._stack.close()
 
     def start(self) -> None:
-        """Starts an event loop, handles user input and updates the canvas until user requests to close the program."""
+        """Starts an event loop, handles user input and updates the canvas until
+        user requests to close the program."""
+
+        write(self.term.clear)
 
         # Show the cursor before the first user keypress
-        self._queue(self.x, self.y)
-        self._flush()
+        self._render(set([self.canvas.cursor]))
 
         while not self._quitting:
             self.update()
@@ -71,66 +107,54 @@ class CanvasController:
             self._quitting = True
             return
 
-        self._update_cursor(key)
-        self._update_blocks(key)
-        self._flush()
+        updates = self._update_cursor(key)
+        updates = updates.union(self._update_blocks(key))
+        self._render(updates)
 
-    def _update_cursor(self, key: str) -> None:
-        """Modifies the cursor position and queues the cursor render according to the pressed key."""
+    def _update_cursor(self, key: str) -> set[tuple[int, int]]:
+        """Updates the cursor position if the key is one of the movement keys.
+
+        Returns:
+            A set of updated positions."""
 
         velocity = (0, 0)
-        if self.y > 0 and key == "w":
-            velocity = (0, -1)
-        if self.y < self.height - 1 and key == "s":
-            velocity = (0, 1)
-        if self.x > 0 and key == "a":
-            velocity = (-1, 0)
-        if self.x < self.width - 1 and key == "d":
-            velocity = (1, 0)
+        match key:
+            case "w":
+                velocity = (0, -1)
+            case "s":
+                velocity = (0, 1)
+            case "a":
+                velocity = (-1, 0)
+            case "d":
+                velocity = (1, 0)
 
-        if velocity[0] != 0 or velocity[1] != 0:
-            prev_x = self.x
-            prev_y = self.y
-            self.x += velocity[0]
-            self.y += velocity[1]
-            self._queue(prev_x, prev_y)
-            self._queue(self.x, self.y)
+        return self.canvas.move_cursor(*velocity)
 
-    def _update_blocks(self, key: str) -> None:
-        """Checks if the pressed key is the key that toggles a block, updates the blocks array accordingly and queues a render of the block."""
+    def _update_blocks(self, key: str) -> set[tuple[int, int]]:
+        """Toggles a block if user pressed the space key.
+
+        Returns:
+            A set of updated positions."""
 
         if key != " ":
-            return
+            return set()
 
-        pos = (self.x, self.y)
-        if pos in self.blocks:
-            self.blocks.remove(pos)
-        else:
-            self.blocks.add(pos)
+        return self.canvas.toggle_block()
 
-        self._queue(self.x, self.y)
-
-    def _queue(self, x: int, y: int) -> None:
-        """Queues an update of the character at the specified coordinates."""
-
-        self.updates.append((x, y))
-
-    def _flush(self) -> None:
-        """Writes all queued updates to the stdout and the queue."""
+    def _render(self, positions: set[tuple[int, int]]) -> None:
+        """Renders the specified set of positions."""
 
         seq = ""
-        for x, y in self.updates:
-            seq += self._point_sequence(x, y)
+        for x, y in positions:
+            seq += self._render_sequence(x, y)
         write(seq)
 
-        self.updates = []
+    def _render_sequence(self, x: int, y: int) -> str:
+        """Creates a render sequence for the specified point."""
 
-    def _point_sequence(self, x: int, y: int) -> str:
-        """Generates a render sequence for the specified point."""
-
-        if self.x == x and self.y == y:
+        if self.canvas.cursor == (x, y):
             return self.term.move_xy(x, y) + self.term.blink(self.cursor)
-        elif (x, y) in self.blocks:
+        elif (x, y) in self.canvas.blocks:
             return self.term.move_xy(x, y) + self.term.teal(self.block)
         else:
             return self.term.move_xy(x, y) + " "
